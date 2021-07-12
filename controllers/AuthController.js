63fab4e84733bf0562b526944e7ddeb79d9028e6
sohamraktawan <sohamraktawan@gmail.com>
2021-07-12 17:32:10 +0530

    first commit

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,6 +1,6 @@
 const User = require("../models/UserModel");
 const jwt = require("jsonwebtoken")
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcryptjs")
 const jsCookie = require("js-cookie")
 
 
