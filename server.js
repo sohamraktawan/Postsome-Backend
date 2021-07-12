2b001a93662c5009904319a0b9fec8028d6692a6
sohamraktawan <sohamraktawan@gmail.com>
2021-07-12 14:21:45 +0530

    make it better

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const authroutes = require('./routes/AuthRoutes')
+const cookieParser = require('cookie-parser')
+require('dotenv').config();
+const dotenv = require('dotenv');
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+
+const dbURI = process.env.db_connect
+
+mongoose.connect(dbURI,{ useNewUrlParser: true, useUnifiedTopology: true })
+.then(res=>{
+    console.log("connected to db")
+})
+.catch(err=>{
+    console.log(err);
+});
+
+app.use("/", require('./routes/postRoute'));
+app.use(authroutes);
+
+
+app.listen(process.env.PORT || 3001, ()=>{
+    console.log("express running is localhost 3001");
+})
