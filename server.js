e69e54237e4e7a01ff20fda617c0268f8b076625
sohamraktawan <sohamraktawan@gmail.com>
2021-07-14 22:35:23 +0530

    app crash resolved

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const authroutes = require('./routes/AuthRoutes')
 const cookieParser = require('cookie-parser')
 require('dotenv').config();
 const dotenv = require('dotenv');
+const path = require('path')
 
 app.use(cors());
 app.use(cookieParser());
@@ -28,3 +29,7 @@ app.use(authroutes);
 app.listen(process.env.PORT || 3001, ()=>{
     console.log("express running is localhost 3001");
 })
+
+app.get('/*',(req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'))
+  })
